108ccbed3caf61e500f3810c6174d60f27596a38
jmejia1920 <jonny86493@gmail.com>
2023-05-12 11:09:44 -0400

    Added dealership and vehicle class

---
+++ src/main/java/com/learntocode/Dealership.java
@@ -0,0 +1,35 @@
+package com.learntocode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    private List<Vehicle> getVehiclesPrice(Double min,Double max){
+        return null;
+    }
+    private void addVehicle(Vehicle vehicle){
+
+    }
+    private List<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+    private List<Vehicle> getVehiclesByYear(Double min, Double max){
+        return null;
+    }
+    private List<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+    private List<Vehicle> getVehiclesByMileage(Double min, Double max){
+        return null;
+    }
+    private List<Vehicle> getVehiclesByType(String vehicleType){
+        return null;
+    }
+
+}
